d69ea721776b479b3d47509411e51c835a7193f8
PythonFabi <129299589+PythonFabi@users.noreply.github.com>
2023-09-09 02:07:01 -0400

    create the Portfoliopage structure

---
+++ src/components/PortfolioPage.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import Navbar from './Navbar';
+import Header from './Header';
+import Footer from './Footer';
+import AboutMe from './pages/AboutMe';
+import Portfolio from './pages/Portfolio';
+import Contact from './pages/Contact';
+import Resume from './pages/Resume';
+
+export default function PortfolioPage() {
+    const [currentPage, setCurrentPage] = useState('AboutMe');
+
+    const renderPage = () => {
+        if (currentPage === 'AboutMe') {
+            return <AboutMe />;
+        }
+        if (currentPage === 'Portfolio') {
+            return <Portfolio />;
+        } 
+        if (currentPage === 'Contact') {
+            return <Contact />;
+        } 
+        return <Resume />;
+    };
+
+    const handlePageChange = (page) => setCurrentPage(page);
+
+    return (
+        <div>
+            <Header> 
+                <Navbar currentPage={currentPage} handlePageChange={handlePageChange}/> 
+            </Header>
+            <Footer />
+            {renderPage()}
+        </div>
+    )
+}
