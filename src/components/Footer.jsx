27cb42571562ed6c3c30de2842b84c7a145aa8bf
PythonFabi <129299589+PythonFabi@users.noreply.github.com>
2023-09-10 23:15:08 -0400

    add linkedin and instagram link and icon

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faInstagram } from "@fortawesome/free-brands-svg-icons";
 
 function Footer () {
     return (
-        <footer className="footer fixed-bottom">
+        <footer className="footer fixed-bottom text-center">
             <a className="icon-links" href="https://github.com/PythonFabi"><FontAwesomeIcon icon={faGithub} /></a>
+            <a className="icon-links" href="https://www.linkedin.com/in/fabian-wagner-70905a281/"><FontAwesomeIcon icon={faLinkedin} /></a>
+            <a className="icon-links" href="https://instagram.com/fabi_wag_?igshid=YmMyMTA2M2Y="><FontAwesomeIcon icon={faInstagram} /></a>
         </footer>
     )
 }
