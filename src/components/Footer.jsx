31e42a4999173d10216e23a26bbb0fb8a90a4032
PythonFabi <129299589+PythonFabi@users.noreply.github.com>
2023-09-10 23:54:55 -0400

    add hover color change for nav links and add twitter icon

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,6 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faGithub } from "@fortawesome/free-brands-svg-icons";
-import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
-import { faInstagram } from "@fortawesome/free-brands-svg-icons";
+import { faGithub, faXTwitter, faLinkedin, faInstagram  } from "@fortawesome/free-brands-svg-icons";
 
 function Footer () {
     return (
@@ -10,6 +8,7 @@ function Footer () {
             <a className="icon-links" href="https://github.com/PythonFabi"><FontAwesomeIcon icon={faGithub} /></a>
             <a className="icon-links" href="https://www.linkedin.com/in/fabian-wagner-70905a281/"><FontAwesomeIcon icon={faLinkedin} /></a>
             <a className="icon-links" href="https://instagram.com/fabi_wag_?igshid=YmMyMTA2M2Y="><FontAwesomeIcon icon={faInstagram} /></a>
+            <a className="icon-links" href="https://github.com/PythonFabi"><FontAwesomeIcon icon={faXTwitter} /></a>
         </footer>
     )
 }
