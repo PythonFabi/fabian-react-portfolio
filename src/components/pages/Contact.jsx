62c921f753fab351aa4c5badbbacff5a85363ab3
PythonFabi <129299589+PythonFabi@users.noreply.github.com>
2023-09-14 15:43:10 -0400

    add contact, header and navbar css files and import them in their corresponding jsx files

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { validateEmail } from '../../utils/helpers';
+import '../../styles/Contact.css';
 
 function Contact() {
   const [name, setName] = useState('');
