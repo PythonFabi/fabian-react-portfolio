2b368af6d33ba0f9ced0814f110436b8e4c676d0
PythonFabi <129299589+PythonFabi@users.noreply.github.com>
2023-09-15 12:53:14 -0400

    add more responsive styling to header and project description

--- src/components/pages/Portfolio.jsx
+++ src/components/pages/Portfolio.jsx
@@ -8,10 +8,10 @@ function Portfolio() {
       <div className="d-flex flex-wrap">
         <Project
           className="fit-with-vids single-project"
-          link="https://pythonfabi.github.io/Fit-with-Vids/"
-          title="Fit with Vids"
-          githublink="https://github.com/PythonFabi/Fit-with-Vids"
-          description="Front-end app with server-side APIs"
+          link="https://warm-caverns-23380-13159eb09b79.herokuapp.com/"
+          title="Mode de Vie"
+          githublink="https://github.com/PythonFabi/mode-de-vie"
+          description="MVC APP"
         />
         <Project
           className="fit-with-vids single-project"
