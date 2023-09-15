d684c42eb23ced229407ffd4e3b31eb8966e455d
PythonFabi <129299589+PythonFabi@users.noreply.github.com>
2023-09-15 14:31:47 -0400

    add remaining projects to portfolio page

--- src/components/pages/Portfolio.jsx
+++ src/components/pages/Portfolio.jsx
@@ -7,7 +7,7 @@ function Portfolio() {
       <h2>Portfolio</h2>
       <div className="d-flex flex-wrap">
         <Project
-          className="fit-with-vids single-project"
+          className="mode-de-vie single-project"
           link="https://warm-caverns-23380-13159eb09b79.herokuapp.com/"
           title="Mode de Vie"
           githublink="https://github.com/PythonFabi/mode-de-vie"
@@ -21,31 +21,31 @@ function Portfolio() {
           description="Front-end app with server-side APIs"
         />
         <Project
-          className="fit-with-vids single-project"
-          link="https://pythonfabi.github.io/Fit-with-Vids/"
-          title="Fit with Vids"
-          githublink="https://github.com/PythonFabi/Fit-with-Vids"
-          description="Front-end app with server-side APIs"
+          className="my-tech-blog single-project"
+          link="https://glacial-fortress-55574-019570e3c30e.herokuapp.com/"
+          title="My Tech Blog"
+          githublink="https://github.com/PythonFabi/my-tech-blog"
+          description="MVC APP"
         />
         <Project
-          className="fit-with-vids single-project"
-          link="https://pythonfabi.github.io/Fit-with-Vids/"
-          title="Fit with Vids"
-          githublink="https://github.com/PythonFabi/Fit-with-Vids"
-          description="Front-end app with server-side APIs"
+          className="social-network-api single-project"
+          link="https://drive.google.com/file/d/1FfFFYJheWD2tk9HWz4Ra5fbUFJvjF424/view"
+          title="Social Network API"
+          githublink="https://github.com/PythonFabi/my-social-network-api"
+          description="Back-end Express.js app with MongoDB"
         />
         <Project
-          className="fit-with-vids single-project"
-          link="https://pythonfabi.github.io/Fit-with-Vids/"
-          title="Fit with Vids"
-          githublink="https://github.com/PythonFabi/Fit-with-Vids"
-          description="Front-end app with server-side APIs"
+          className="employee-tracker single-project"
+          link="https://drive.google.com/file/d/1J8Eac3SRjXf4tKRDQbD1gBVwkK4TMqTl/view"
+          title="Employee Tracker"
+          githublink="https://github.com/PythonFabi/my-employee-tracker"
+          description="Node.js App with SQL"
         />
         <Project
-          className="fit-with-vids single-project"
-          link="https://pythonfabi.github.io/Fit-with-Vids/"
-          title="Fit with Vids"
-          githublink="https://github.com/PythonFabi/Fit-with-Vids"
+          className="my-weather-dashboard single-project"
+          link="https://pythonfabi.github.io/my-weather-dashboard/"
+          title="Weather dashboard"
+          githublink="https://github.com/PythonFabi/my-weather-dashboard"
           description="Front-end app with server-side APIs"
         />
       </div>
